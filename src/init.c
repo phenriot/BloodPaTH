ef27eb67ed87c63440853447b3e13580161d44db
phenriot <95306922+phenriot@users.noreply.github.com>
2024-09-18 13:32:04 +0200

    Update init.c

--- src/init.c
+++ src/init.c
@@ -10,8 +10,4 @@
 /* .Call calls */
 extern SEXP _BloodPatH_BloodPatH_model(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP _BloodPatH_BloodPatH_model_inter(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP _BloodPatH_rcpparma_bothproducts(SEXP);
-extern SEXP _BloodPatH_rcpparma_hello_world();
-extern SEXP _BloodPatH_rcpparma_innerproduct(SEXP);
-extern SEXP _BloodPatH_rcpparma_outerproduct(SEXP);
 extern SEXP _BloodPatH_transform_list(SEXP, SEXP);
